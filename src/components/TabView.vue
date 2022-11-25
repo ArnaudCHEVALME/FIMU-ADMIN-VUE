a1bba4b7429266e533e961974dfb558440517830
Nono <arnaud.c4522@protonmail.com>
2022-11-25 09:20:24 +0100

    truc de base

---
+++ src/components/TabView.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-simple-table>
+    <tr>
+      <thead>hello</thead>
+    </tr>
+  </v-simple-table>
+</template>
+
+<script>
+export default {
+  name: "TabView"
+}
+</script>
+
+<style scoped>
+
+</style>
