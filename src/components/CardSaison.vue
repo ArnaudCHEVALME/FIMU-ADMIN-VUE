39a67ff081d8040572b36553590eeb4b73f8aead
Samson Dupuy <samson.dupuy@edu.univ-fcomte.fr>
2022-11-25 09:26:38 +0100

    saisons card

---
+++ src/components/CardSaison.vue
@@ -0,0 +1,46 @@
+<template>
+  <container>
+    <v-card v-for="(saison, id) in saisons" :key="id">
+      <v-img src="{{ saison.image }}"></v-img>
+        <v-card-text>
+          {{ saison.date }}: {{ saison.theme }}
+        </v-card-text>
+      <v-card-subtitle>
+        <v-button>Le boutton</v-button>
+      </v-card-subtitle>
+    </v-card>
+  </container>
+</template>
+
+<script>
+export default {
+  name: "CardSaison",
+  data: () => ({
+    saisons: [
+      {
+        date: "22/10/2022",
+        theme: "Shrek",
+        image: "./assets/logo.png",
+        paysId: 1,
+        pays: {
+          paysId: 1,
+          nomPays: "Zimbabwe"
+        }
+      }, {
+        date: "23/10/2022",
+        theme: "Imaginaire",
+        image: "./assets/logo.png",
+        paysId: 2,
+        pays: {
+          paysId: 2,
+          nomPays: "Listembourg"
+        }
+      }
+    ]
+  })
+};
+</script>
+
+<style scoped>
+
+</style>
