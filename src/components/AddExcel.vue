14253258f7476737bf0bbe98a005c4cb320e2574
LilianSchott <114645414+LilianSchott@users.noreply.github.com>
2022-12-08 16:27:52 +0100

    Create AddExcel.vue
    bouton ajouter l'Excel

---
+++ src/components/AddExcel.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-container>
+        <v-col>
+            <v-row>
+                <label for="doc">Choisissez un document </label>
+                <input type="file" id="doc" name="doc" accept=".xlsm">
+            </v-row>
+            <v-btn>Envoyer</v-btn>
+        </v-col>
+        
+    </v-container>
+
+</template>
+
+<script>
+import { component } from 'vue/types/umd';
+
+export default {
+    name: "AddDoc",
+
+    components: { component }
+    
+}
+</script>
